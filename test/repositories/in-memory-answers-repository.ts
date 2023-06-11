874101602bf911137ce1b1edb6ff8ba0bf381065
wedersonf <wedymf@gmail.com>
2023-06-11 04:11:30 -0300

    feat: edit answer use case

--- test/repositories/in-memory-answers-repository.ts
+++ test/repositories/in-memory-answers-repository.ts
@@ -14,12 +14,18 @@ export class InMemoryAnswersRepository implements AnswersRepository {
     return answer
   }
 
+  async save(answer: Answer) {
+    const itemIndex = this.items.findIndex((item) => item.id === answer.id)
+
+    this.items[itemIndex] = answer
+  }
+
   async create(answer: Answer) {
     this.items.push(answer)
   }
 
-  async delete(question: Answer) {
-    const itemIndex = this.items.findIndex((item) => item.id === question.id)
+  async delete(answer: Answer) {
+    const itemIndex = this.items.findIndex((item) => item.id === answer.id)
 
     this.items.splice(itemIndex, 1)
   }
